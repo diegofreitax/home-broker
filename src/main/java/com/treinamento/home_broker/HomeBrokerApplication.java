1945c874522e2641dcd9e7ba7555f6df5804e706
Diego Freitas <diegobfreitas23@gmail.com>
2026-01-21 10:52:05 -0300

    Initial commit

---
+++ src/main/java/com/treinamento/home_broker/HomeBrokerApplication.java
@@ -0,0 +1,13 @@
+package com.treinamento.home_broker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeBrokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeBrokerApplication.class, args);
+	}
+
+}
